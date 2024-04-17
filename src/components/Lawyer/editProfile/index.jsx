906fe4f8964251f89eeba43a2a3ea0b7834e6d5e
shehryar <shehryarkashan@gmail.com>
2024-04-17 07:23:28 +0500

    change

--- src/components/Lawyer/editProfile/index.jsx
+++ src/components/Lawyer/editProfile/index.jsx
@@ -29,6 +29,27 @@ const educationSchema = yup.object().shape({
   endYear: yup.number().required("end yer is required"),
 });
 
+const availabilitySchema = yup.object().shape({
+  days: yup
+    .array()
+    .of(yup.string())
+    .required("At least one day is required")
+    .min(1, "At least one day is required")
+    .test("days", "Invalid weekday", (value) =>
+      value.every((day) =>
+        [
+          "monday",
+          "tuesday",
+          "wednesday",
+          "thursday",
+          "friday",
+          "saturday",
+          "sunday",
+        ].includes(day.toLowerCase())
+      )
+    ),
+});
+
 const EditProfile = () => {
   const [value, setValue] = useState([]);
   const {
@@ -69,7 +90,18 @@ const EditProfile = () => {
     },
   });
 
-  // console.log(data);
+  const {
+    register: registerAvailability,
+    handleSubmit: handleSubmitAvailability,
+    formState: { errors: availabilityErrors },
+    setValue: setAvailabilityValue,
+  } = useForm({
+    resolver: yupResolver(availabilitySchema),
+    defaultValues: {
+      days: data?.LawyerProfile?.availability?.days || [],
+    },
+  });
+
   const [completeLawyerProfile, { isError, error, isLoading }] =
     useCompleteLawyerProfileMutation();
 
@@ -114,6 +146,8 @@ const EditProfile = () => {
       "endYear",
       data?.LawyerProfile?.education?.completionYear?.endYear || ""
     );
+    setAvailabilityValue("days", data?.LawyerProfile?.availability?.days || []);
+    setValue(data?.LawyerProfile?.availability?.days || []);
   }, [data]);
 
   const submitProfessionalInfo = async (data) => {
@@ -153,7 +187,7 @@ const EditProfile = () => {
     }),
     container: {
       className: classNames(
-        "md:p-0.8 p-lg m-0 list-none cursor-text overflow-hidden focus:border-2 flex items-center flex-wrap",
+        "md:p-0.8 p-lg m-0  gap list-none cursor-text overflow-hidden focus:border-2 flex items-center flex-wrap",
         "w-full",
         "font-sans text-base text-gray-600 bg-white border border-black acti transition-colors duration-200 appearance-none small-btn-border-radius",
         "hover:border-black hover:border-2 focus:outline-0"
@@ -161,22 +195,54 @@ const EditProfile = () => {
     },
     inputToken: {
       className: classNames(
-        "focus:outline-0 active:outline-0 outline-0 flex flex-1 inline-flex"
+        "focus:outline-0 active:outline-0 outline-0 flex gap flex-1 inline-flex"
       ),
     },
     input: {
       className: classNames(
-        "text-base text-gray-700 w-full p-0 border-none ring-0 ",
+        "text-base text-gray-700 w-full p-0 border-none ring-0  ",
         ""
       ),
     },
     token: {
       className: classNames(
-        "md:p-0.5 p-lg md:mr-lg mr-md bg-gray-300 dark:bg-gray-700 text-gray-700 md:text-base text-sm small-btn-border-radius",
-        "cursor-default inline-flex items-center "
+        "md:p-0.5 p-lg md:mr-lg mr-md  bg-gray-300 dark:bg-gray-700 text-gray-700 md:text-base text-sm small-btn-border-radius",
+        "inline-flex items-center cursor-pointer "
       ),
     },
     removeTokenIcon: "ml-0.8 ",
+  };
+  const isWeekday = (input) => {
+    const weekdays = [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday",
+      "saturday",
+      "sunday",
+    ];
+    return weekdays.includes(input.toLowerCase());
+  };
+
+  const handleChange = (e) => {
+    const newValue = e.value.filter(isWeekday);
+    setValue(newValue);
+    setAvailabilityValue("days", newValue);
+  };
+
+  const submitAvailability = async (data) => {
+    try {
+      const response = await completeLawyerProfile(data);
+      if (response?.data?.success) {
+        Swal.fire({
+          icon: "success",
+          title: "Profile Updated",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <Tabs
@@ -344,7 +410,13 @@ const EditProfile = () => {
                 </div>
               </div>
               <button type="submit" className="gig-btn">
-                update
+                {isLoading ? (
+                  <div className="item-center">
+                    <Loader />
+                  </div>
+                ) : (
+                  "update"
+                )}
               </button>
             </form>
           </div>
@@ -354,24 +426,46 @@ const EditProfile = () => {
             <h3 className="lg:text-xl md:text-lg text-base font-extrabold capitalize">
               update your professional info
             </h3>
-            <div className="f-col justify-between h-full ">
-              <div className="f-col gap">
-                <label htmlFor={"days"} class="input-lable">
-                  Days
-                </label>
-                <Chips
-                  value={value}
-                  onChange={(e) => setValue(e.value)}
-                  placeholder={
-                    value.length < 6 ? "Enter the days you are available" : ""
-                  }
-                  variant="outlined"
-                  max={6}
-                  pt={chipsTheme}
-                />
+            <form
+              className="f-col justify-between h-full "
+              onSubmit={handleSubmitAvailability(submitAvailability)}
+            >
+              <div className="h-full f-col gap">
+                <div className="f-col gap h-full">
+                  <label htmlFor={"days"} class="input-lable">
+                    Days
+                  </label>
+                  <div className="f-col gap">
+                    <Chips
+                      value={value}
+                      onChange={handleChange}
+                      name="days"
+                      keyfilter={/^[a-zA-Z]+$/}
+                      placeholder={
+                        value.length < 6
+                          ? "Enter the days you are available"
+                          : ""
+                      }
+                      variant="outlined"
+                      max={6}
+                      pt={chipsTheme}
+                    />
+                    {availabilityErrors.days && (
+                      <FailureAlert error={availabilityErrors.days.message} />
+                    )}
+                  </div>
+                </div>
+                <button type="submit" className="gig-btn">
+                  {isLoading ? (
+                    <div className="item-center">
+                      <Loader />
+                    </div>
+                  ) : (
+                    "update"
+                  )}
+                </button>
               </div>
-            </div>
-            <button className="gig-btn">update</button>
+            </form>
           </div>
         </TabPanel>
       </div>
