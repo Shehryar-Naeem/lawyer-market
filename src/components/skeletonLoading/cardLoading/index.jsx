81beaf2eda7d8c4674e4a98f5cac34d05c890410
Shehryar <shehryarkashan@gmail.com>
2024-05-23 16:12:22 +0500

    change

--- src/components/skeletonLoading/cardLoading/index.jsx
+++ src/components/skeletonLoading/cardLoading/index.jsx
@@ -15,7 +15,8 @@ const CardSkeletonLoading = () => {
           <div className="flex md:gap-1 sm:gap-sm gap-xs items-center">
             <Skeleton
               circle
-              className="lg:w-sm-img md:w-xs-img lg:h-sm-img md:h-xs-img w-[30px] h-[30px] p-0"
+            
+              className="lg:min-w-sm-img md:min-w-xs-img lg:min-h-sm-img md:min-h-xs-img min-w-[30px] min-h-[30px] p-0"
             />
             <div className="w-full">
               <Skeleton className="lg:h-4 md:h-3 h-2 md:rounded-lg sm:rounded-md rounded-sm p-0" />
